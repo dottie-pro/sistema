194b077ddd6cb35f97bbbed23c530beca1aeb99e
rodrigoschmiegelow <schmi@dottie.pro>
2024-12-03 13:02:06 -0300

    Update help.tsx

--- src/help.tsx
+++ src/help.tsx
@@ -2,7 +2,7 @@ export const formatCPF = async (cpf: string) => {
 
     const cleanedCPF = cpf.replace(/\D/g, '');
     const cpfRegex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/;
-    const formattedCPF = cleanedCPF.replace(cpfRegex, '$1.$2.$3-$4');
+    const formattedCPF = cleanedCPF.replace(cpfRegex, '$1.$2.$3-$4'); 
 
     return formattedCPF;
 }
